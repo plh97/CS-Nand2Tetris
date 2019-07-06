33f491c8e75e90f3e24b02c8c1b3e89038a4de2d
彭立衡 <pengliheng111@gmail.com>
2019-07-06 13:09:03 +0800

    feat: repeat to do this homework because i can't finish chapter 5

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,27 +6,4 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-@R2
-M=0 // 初始化M值
-
-@R0
-D=M
-@Times
-M=D
-// loop
-(LOOP)
-@Times
-D=M
-@END
-D,JEQ
-@R1
-D=M
-@R2
-M=M+D
-@Times
-M=M-1
-@LOOP
-0,JMP
-(END)
-// end
 
